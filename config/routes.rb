1894b3a6f78493832719c0c2b7937248fe6b6083
Aurore Courtois <aurore.courtois@outlook.fr>
2017-05-18 17:03:32 +0200

    Mr. Cocktail Day 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "cocktails#index"
+
+  resources :cocktails, except: [ :destroy, :edit, :update ] do
+    resources :doses, only: [ :new, :create ]
+  end
+
+  resources :doses, only: [ :destroy ]
+  #resources :cocktails, except: [ :destroy, :edit, :update ] do
+    #member do
+   #   get 'ingredients', to: "cocktails#ingredients"
+    #end
+  #end
 end
